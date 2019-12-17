a81ba1c4d45ed42b832d95e21662047f56c7f48d
lilcoffee <luk.kawka@gmail.com>
2019-12-17 20:11:51 +0100

    Lab10Ex1

--- src/components/PageEmployeeCreate.jsx
+++ src/components/PageEmployeeCreate.jsx
@@ -39,44 +39,45 @@ class PageEmployeeCreate extends React.Component {
 
   createEmployee() {
     this.setState({ isSaving: true, error: null });
-    
-    const { 
+
+    const {
       name,
-      age, 
-      company, 
+      age,
+      company,
       email,
     } = this.state;
 
-    const employee = { 
+    const employee = {
       id: Date.now(),
-      name, 
-      age, 
-      company, 
-      email };
+      name,
+      age,
+      company,
+      email
+    };
 
     fetch('http://localhost:3004/employees', {
-      method: 'POST', 
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       },
       body: JSON.stringify(employee)
     })
-    .then(res => {
-      if(res.status !== 201) {
-        this.setState({ isSaving: false, error: `Saving returned status ${res.status}`})
-      } else {
-        this.props.history.push("/");
-      }
-    })  
+      .then(res => {
+        if (res.status !== 201) {
+          this.setState({ isSaving: false, error: `Saving returned status ${res.status}` })
+        } else {
+          this.props.history.push("/");
+        }
+      })
   }
 
   render() {
-    const { 
-      name, 
-      age, 
-      company, 
-      email, 
+    const {
+      name,
+      age,
+      company,
+      email,
       isSaving,
       error,
     } = this.state;
@@ -85,9 +86,9 @@ class PageEmployeeCreate extends React.Component {
       <div>
         <h1>Enter employees data:</h1>
         <div>Name: <input type="text" value={name} onChange={this.nameChanged} disabled={isSaving} /></div>
-        <div>Age: <input type="number" value={age} onChange={this.ageChanged} disabled={isSaving}/></div>
-        <div>Company: <input type="text" value={company} onChange={this.companyChanged} disabled={isSaving}/></div>
-        <div>Email: <input type="email" value={email} onChange={this.emailChanged} disabled={isSaving}/></div>
+        <div>Age: <input type="number" value={age} onChange={this.ageChanged} disabled={isSaving} /></div>
+        <div>Company: <input type="text" value={company} onChange={this.companyChanged} disabled={isSaving} /></div>
+        <div>Email: <input type="email" value={email} onChange={this.emailChanged} disabled={isSaving} /></div>
         {!isSaving ? <button onClick={this.createEmployee}>Create employee</button> : <p>Saving ...</p>}
         {error && <p>An error occured: {error}</p>}
       </div>
@@ -95,4 +96,10 @@ class PageEmployeeCreate extends React.Component {
   }
 }
 
-export default withRouter(PageEmployeeCreate);
+const mapStateToProps = (state) => {
+  return {};
+}
+
+const mapDispatchToProps = (dispatch) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(PageEmployeeCreate));
